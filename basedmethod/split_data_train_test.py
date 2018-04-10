d3eb25e71876789e3a91a9af3ea0e1edc0f3540c
run wang <runwang@runmatoMacBook-Pro.local>
2018-04-10 11:01:39 -0400

    s

--- basedmethod/split_data_train_test.py
+++
@@ -1,3 +0,0 @@
-from util import split_train_test
-import sys
-split_train_test(sys.argv[1])
--- basedmethod/split_data_train_validation_test.py
+++
@@ -1,3 +0,0 @@
-from util import split_train_validation_test
-import sys
-split_train_validation_test(sys.argv[1])
